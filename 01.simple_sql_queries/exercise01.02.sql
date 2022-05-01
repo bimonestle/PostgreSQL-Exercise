dbc745938d24f6fd766663eec22632cc67cfc710
bimonestle <bimonestle@yahoo.co.id>
2022-05-01 13:55:03 +0700

    Retrieve specific columns from a table

---
+++ 01.simple_sql_queries/exercise01.02.sql
@@ -0,0 +1,7 @@
+-- RETRIEVE SPECIFIC COLUMNS FROM A TABLE
+
+-- You want to print out a list of all of the facilities name
+-- and their cost to members. How would you retrieve a list of
+-- only facility names and costs?
+
+SELECT name, membercost FROM cd.facilities;
