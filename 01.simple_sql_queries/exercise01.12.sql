42c3cc7acc2c227baa29852ff6e7d4cb868e0de4
bimonestle <bimonestle@yahoo.co.id>
2022-05-08 18:42:53 +0700

    additional note

--- 01.simple_sql_queries/exercise01.12.sql
+++ 01.simple_sql_queries/exercise01.12.sql
@@ -6,6 +6,8 @@
 -- My answer
 -- SELECT firstname, surname, joindate FROM cd.members
 -- ORDER BY joindate DESC LIMIT 1;
+-- "Note that this approach does not cover
+-- the extremely unlikely eventuality of two people joining at the exact same time :-)"
 
 -- Proper answer
 SELECT firstname, surname, joindate FROM cd.members
