978412e145512fa1fe7feb40aab6159755f7cda8
bimonestle <bimonestle@yahoo.co.id>
2022-05-08 18:24:13 +0700

    Combine results from multiple queries

---
+++ 01.simple_sql_queries/exercise01.10.sql
@@ -0,0 +1,8 @@
+-- COMBINING RESULTS FROM MULTIPLE QUERIES
+
+-- You, for some reason, want a combined list of all surnames and all facility names.
+-- Yes, this is a contrived example :-). Produce that list!
+
+SELECT surname FROM cd.members
+UNION
+SELECT name FROM cd.facilities;
