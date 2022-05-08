4b15389ecc4e929ab029236b4e47f263d08d71e3
bimonestle <bimonestle@yahoo.co.id>
2022-05-08 18:19:58 +0700

    Removing duplicates and ordered results

---
+++ 01.simple_sql_queries/exercise01.09.sql
@@ -0,0 +1,7 @@
+-- REMOVING DUPLICATES, and ORDERING RESULTS
+
+-- How can you produce an ordered list of the first 10 surnames in the members table?
+-- The list must not contain duplicates.
+
+SELECT DISTINCT surname FROM cd.members
+ORDER BY surname LIMIT 10;
